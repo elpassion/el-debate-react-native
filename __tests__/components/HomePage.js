6179060900ff239da09ecf11e1a0690be5acf886
Karol Wyliziński <k.wylizinski@wp.pl>
2017-10-08 10:59:54 +0200

    Use Enzyme and Enzyme serializer for testing, Add snapshot test for DebateDetails component, update HomePage snapshot

--- __tests__/components/HomePage.js
+++ __tests__/components/HomePage.js
@@ -1,12 +1,16 @@
 import 'react-native';
 import React from 'react';
+
 import HomePage from 'app/components/HomePage/HomePage';
 
-import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <HomePage />
-    ).toJSON();
+  const tree = shallow(<HomePage />)
   expect(tree).toMatchSnapshot();
 });
