f99f4bb32241aff81471e49014dd7a05f5b96f90
Karol Wyliziński <k.wylizinski@wp.pl>
2017-10-26 20:35:03 +0200

    Add Comment screen layout

--- app/components/Comments/Comments.js
+++ app/components/Comments/Comments.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react'
-import { View } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 
 import styles from './styles'
 import Navbar from '/app/components/Shared/Navbar'
+import Svg, { Circle } from 'react-native-svg';
 
 export default class Comments extends Component {
   render() {
+    const singleComment = (color, isCurrentUserComment, comment, commentTime, userName, userInitials) =>
+      <View style={{backgroundColor: isCurrentUserComment ? '#f7f7f7' : 'transparent'}}>
+        <View style={styles.comment}>
+          <Text style={styles.userInitials}>{userInitials}</Text>
+          <Svg height="100" width="100" style={styles.circle}>
+            <Circle cx="35" cy="40" r="23" fill={color}/>
+          </Svg>
+          <View style={styles.commentBox}>
+            <Text style={styles.commentUser}>{userName}</Text>
+            <Text style={styles.commentText}>{comment}</Text>
+          </View>
+          <View style={styles.timeBox}>
+            <Text style={styles.time}>{commentTime}</Text>
+          </View>
+        </View>
+        <View style={styles.commentDash}/>
+      </View>
     return (
-      <View style={styles.navContainer}>
-        <Navbar history={this.props.history}/>
+      <View>
+        <View style={styles.navContainer}>
+          <Navbar history={this.props.history}/>
+        </View>
+        <ScrollView style={styles.comments}>
+        </ScrollView>
       </View>
     )
   }
--- app/components/Comments/styles.js
+++ app/components/Comments/styles.js
@@ -1,7 +1,71 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 
 export default StyleSheet.create({
   navContainer: {
-    flex: 1
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    zIndex: 2
+  },
+  comments: {
+    top: (Platform.OS === 'ios') ? 70 : 0,
+    left: 0,
+    right: 0,
+    zIndex: 0,
+    height: '100%'
+  },
+  comment: {
+    top: 0,
+    height: 80,
+    marginLeft: 0,
+    marginRight: 15,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  userInitials: {
+    position: 'absolute',
+    marginTop: 33,
+    marginLeft: 25,
+    fontFamily: 'helvetica-neue-md',
+    fontSize: 12,
+    zIndex: 2,
+    backgroundColor: 'transparent',
+    color: 'white'
+  },
+  circle: {
+    zIndex: 1
+  },
+  commentBox: {
+    marginRight: 80,
+    marginTop: 5
+  },
+  commentUser: {
+    position: 'relative',
+    marginTop: 15,
+    left: -30,
+    fontFamily: 'helvetica-neue-md'
+  },
+  commentText: {
+    position: 'relative',
+    marginTop: 5,
+    left: -30,
+    marginRight: 30
+  },
+  timeBox: {
+    marginTop: 20,
+    right: 0,
+    top: 0,
+    position: 'absolute'
+  },
+  time: {
+    color: '#B8B8B8'
+  },
+  commentDash: {
+    borderBottomColor: '#f2f2f2',
+    borderBottomWidth: 1,
+    width: '90%',
+    alignSelf: 'center',
+    height: 1
   }
 })
